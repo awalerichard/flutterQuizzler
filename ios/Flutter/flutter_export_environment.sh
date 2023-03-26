495257512bb862f5653255a14de21f0ead4fae51
awalerichard <richardawale182@gmail.com>
2023-03-26 20:27:02 +0300

    test

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/Users/richardawale/Desktop/Flutter/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/richardawale/Desktop/Flutter/quizzler-flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/richardawale/Desktop/Flutter/flutterQuizzler"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
-export "FLUTTER_TARGET=/Users/richardawale/Desktop/Flutter/quizzler-flutter/lib/main.dart"
+export "FLUTTER_TARGET=/Users/richardawale/Desktop/Flutter/flutterQuizzler/lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=1"
@@ -11,4 +11,4 @@ export "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
 export "DART_OBFUSCATION=false"
 export "TRACK_WIDGET_CREATION=true"
 export "TREE_SHAKE_ICONS=false"
-export "PACKAGE_CONFIG=/Users/richardawale/Desktop/Flutter/quizzler-flutter/.dart_tool/package_config.json"
+export "PACKAGE_CONFIG=/Users/richardawale/Desktop/Flutter/flutterQuizzler/.dart_tool/package_config.json"
